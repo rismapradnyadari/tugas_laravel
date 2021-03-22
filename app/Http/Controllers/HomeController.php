ea221b97ab33cb71eb7e6af7f77fbc9bd056c093
rismapradnyadari <rismapradnyadariii@gmail.com>
2021-03-22 19:46:43 +0800

    tugas_laravel

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Facade\FlareClient\View;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    //
+    public function index(){
+
+        return view('admin.beranda');
+    }
+   
+}
